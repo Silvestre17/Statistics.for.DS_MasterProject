34483ff2dd3eba6fcbeb0c79adc1a5790edbd94e
André Silvestre <andre.f.g.silvestre@gmail.com>
2024-11-27 11:13:38 +0000

    Teste de Commit

---
+++ SfDS_Project.R
@@ -0,0 +1,28 @@
+################################################################################
+#                Statistics for Data Science Project - Group XX                #
+#                                                                              #
+# Authors:                                                                     #
+# - André Filipe Gomes Silvestre (20240502)                                    #
+# - Umeima Adam Mahomed (20240543)                                             #
+################################################################################
+
+# Load libraries
+install.packages("readxl")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("ggmap")
+install.packages("maps")
+install.packages("mapdata")
+
+library("tidyverse")
+
+
+
+# Load data
+missing_migrants <- read_excel("./Missing_Migrants_Global_Figures_allData.xlsx")
+
+
+
+
+
